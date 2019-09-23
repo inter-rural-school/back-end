d00afa65f34e7ca71c69942b92aaae4b0753a782
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-23 11:43:21 -0500

    Postgress

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require("./api/server.js");
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
