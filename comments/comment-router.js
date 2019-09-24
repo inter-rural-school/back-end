3ade85fd5fc46f9646d886cc23e491376d7447e9
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-24 17:36:16 -0500

    Added get comments with issues endpoints for board members

--- comments/comment-router.js
+++ comments/comment-router.js
@@ -3,6 +3,16 @@ const express = require('express');
 const Comments = require('./comment-model.js');
 const router = express.Router();
 
+router.get('/', (req,res) => {
+    Comments.getComment()
+    .then(comment => {
+        res.status(200).json(comment)
+    })
+    .catch (err => {
+        res.status(500).json({ message: 'Failed to get comment on issue' });
+    });
+})
+
 router.post('/', (req, res) => {
     const comment = req.body;
     Comments.createComment( comment)
